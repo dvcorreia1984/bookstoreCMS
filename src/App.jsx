1fa6c5781fa817d21ae0c9728022d461f884f5b7
dvcorreia1984 <victor@onsleer.co.za>
2023-08-23 16:32:08 +0200

    Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Bookstore</h1>
+      <p>Click on the Vite and React logos to learn more</p>
+    </>
+  )
+}
+
+export default App
