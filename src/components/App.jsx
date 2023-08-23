c44d0272624ef9573897283927d97d653760e676
dvcorreia1984 <victor@onsleer.co.za>
2023-08-23 18:09:07 +0200

    Linters setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import '../styles/App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/quote" element={<QuotePage />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/calculator" element={<CalculatorPage />} />
+    //     <Route path="/quote" element={<QuotePage />} />
+    //   </Routes>
+    // </BrowserRouter>
     <>
-      <h1 className='text-xl font-serif text-red-500 '>Bookstore</h1>
-      <p>Click on the Vite and React logos to learn more</p>
+      <h1 className='font-montserrat text-3xl font-bold text-blue-500'>Bookstore CMS</h1>
+      <h2>BOOKS</h2>
+      <h2>CATEGORIES</h2>
+      <button></button>
     </>
   )
 }
