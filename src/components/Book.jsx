8106a457b70d3f1010ac2de408f824fd80f2fb4a
dvcorreia1984 <victor@onsleer.co.za>
2023-08-24 18:54:37 +0200

    Updated Book.jsx file

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,40 @@
 const Book = () => (
   <>
-    <p>Category</p>
-    <h2>Title</h2>
-    <p>Author</p>
+    <div id="wrapper" className="border border-gray flex flex-row bg-white justify-evenly">
+      <div id="firstThird">
+        <div>
+          <p>Action</p>
+          <h2>The Hunger Games</h2>
+          <p>Suzanne Collins</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <div>
+            <button type="button">Comments</button>
+          </div>
+          <div>|</div>
+          <div>
+            <button type="button">Remove</button>
+          </div>
+          <div>|</div>
+          <div>
+            <button type="button">Edit</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row" id="secondThird">
+        <img src="#" alt="book cover" />
+        <div className="flex flex-col justify-center items-center">
+          <h1>64%</h1>
+          <h2>Completed</h2>
+        </div>
+      </div>
+
+      <div className="font-serif" id="lastThird">
+        <h3 className="text-gray-300">CURRENT CHAPTER</h3>
+        <p>Chapter 17</p>
+        <button className="text-white bg-blue-500 rounded p-2" type="button">UPDATE PROGRESS</button>
+      </div>
+    </div>
   </>
 );
 export default Book;
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,8 +3,6 @@ import Book from "./Book";
 const BookList = () => (
   <div className="p-5 m-5">
     <Book />
-    <Book />
-    <Book />
   </div>
 );
 
