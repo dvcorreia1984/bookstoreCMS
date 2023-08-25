97b2991592aff385139486f24087c88108abb3c0
dvcorreia1984 <victor@onsleer.co.za>
2023-08-25 19:52:36 +0200

    Updated BookList.jsx file

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,7 +3,23 @@ import Book from './Book';
 import Form from './Form';
 
 const BookList = () => {
-  const [bookList, setBookList] = useState([]);
+  const [bookList, setBookList] = useState([
+    {
+      id: '1',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: '2',
+      title: 'Harry Potter and the Order of the Phoenix',
+      author: 'J.K. Rowling',
+    },
+    {
+      id: '3',
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+    },
+  ]);
 
   const addBook = (newBook) => {
     setBookList([...bookList, newBook]);
@@ -20,7 +36,9 @@ const BookList = () => {
           <Book key={book.id} book={book} removeBook={removeBook} />
         ))}
       </div>
-      <Form addBook={addBook} />
+      <div className="p-5 m-5">
+        <Form addBook={addBook} />
+      </div>
     </div>
   );
 };
