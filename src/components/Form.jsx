2ca3cd7d9bafdf71ffd081ba30ffad081f34a64f
dvcorreia1984 <victor@onsleer.co.za>
2023-08-25 12:08:51 +0200

    Added Logic to Form.jsx file

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,36 @@
-const Form = () => (
-  <div>
-    <h3>Add new Book</h3>
-    <form>
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="author" />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, author);
+    const newBook = { title, author };
+    console.log(newBook);
+  };
+
+  return (
+    <div>
+      <h3>Add new Book</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
