64eafefcbb03550dce0a71689769a5da91ed50f4
dvcorreia1984 <victor@onsleer.co.za>
2023-08-25 20:32:17 +0200

    Updated Author.jsx file

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,6 +1,9 @@
 const Author = () => (
   <>
-    <h1>Under Construction</h1>
+    <div className="flex flex-col items-center w-10/12 m-auto">
+      <h1 className="p-10 text-xl ">Under Construction</h1>
+      <img id="myGif" src="https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif" alt="A GIF" />
+    </div>
   </>
 );
 
