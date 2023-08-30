c670b81c69b2b2f1f108b3faa1d7918da1cb8070
dvcorreia1984 <victor@onsleer.co.za>
2023-08-30 14:15:52 +0200

    Updated Form.js file to use local state before form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/booksSlice'; // Import the action creator
+import { addBook } from '../redux/books/booksSlice';
 
 const Form = () => {
-  const dispatch = useDispatch(); // Initialize useDispatch
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const title = e.target.title.value;
-    const author = e.target.author.value;
 
     if (!title || !author) {
       return;
     }
 
-    const newBook = { item_id: uuidv4(), title, author };
-    dispatch(addBook(newBook)); // Dispatch the addBook action with the new book
+    const newBook = {
+      item_id: uuidv4(),
+      title,
+      author,
+      category: 'fiction',
+    };
 
-    e.target.title.value = '';
-    e.target.author.value = '';
+    dispatch(addBook(newBook));
+
+    setTitle('');
+    setAuthor('');
   };
 
   return (
     <div>
       <h3>Add new Book</h3>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="title" />
-        <input type="text" name="author" placeholder="author" />
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
