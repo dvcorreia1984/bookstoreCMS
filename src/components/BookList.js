dca08ad97c10789ce9fd208237a9c953ed5503d8
dvcorreia1984 <victor@onsleer.co.za>
2023-08-30 19:01:18 +0200

    Updated BookList.js file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,20 +2,24 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
-import { getBooks } from '../redux/books/booksSlice'; // Adjust the import path
+import { getBooks } from '../redux/books/booksSlice';
 
 const BookList = () => {
   const dispatch = useDispatch();
-  const books = useSelector((state) => state.books.data);
+  const booksObject = useSelector((state) => state.books.data); // Get the books object
 
   useEffect(() => {
     dispatch(getBooks());
   }, [dispatch]);
 
+  const booksArray = Object.values(booksObject).reduce(
+    (acc, currentValue) => acc.concat(currentValue), [],
+  );
+
   return (
     <div>
       <h2>Book List</h2>
-      {books.map((book) => (
+      {booksArray.map((book) => (
         <Book key={book.item_id} book={book} />
       ))}
       <Form />
