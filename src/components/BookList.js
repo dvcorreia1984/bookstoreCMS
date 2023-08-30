df0cfa11d547106b1611ce55871f62871b9c5f50
dvcorreia1984 <victor@onsleer.co.za>
2023-08-30 11:14:41 +0200

    Integrated booksSlice.js and BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,44 +1,27 @@
-import React, { useState } from 'react';
-import Book from './Book';
-import Form from './Form';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const BookList = () => {
-  const [bookList, setBookList] = useState([
-    {
-      id: '1',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: '2',
-      title: 'Harry Potter and the Order of the Phoenix',
-      author: 'J.K. Rowling',
-    },
-    {
-      id: '3',
-      title: 'To Kill a Mockingbird',
-      author: 'Harper Lee',
-    },
-  ]);
-
-  const addBook = (newBook) => {
-    setBookList([...bookList, newBook]);
-  };
-
-  const removeBook = (id) => {
-    setBookList(bookList.filter((book) => book.id !== id));
-  };
+  const books = useSelector((state) => state.books);
 
   return (
     <div>
-      <div className="p-5 m-5">
-        {bookList.map((book) => (
-          <Book key={book.id} book={book} removeBook={removeBook} />
+      <h2>Book List</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <strong>Title:</strong>
+            {' '}
+            {book.title}
+            {' '}
+            |
+            {' '}
+            <strong>Author:</strong>
+            {' '}
+            {book.author}
+          </li>
         ))}
-      </div>
-      <div className="p-5 m-5">
-        <Form addBook={addBook} />
-      </div>
+      </ul>
     </div>
   );
 };
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,6 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = [
+  {
+    id: '1',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: '2',
+    title: 'Harry Potter and the Order of the Phoenix',
+    author: 'J.K. Rowling',
+  },
+  {
+    id: '3',
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+  },
+];
 
 const booksSlice = createSlice({
   name: 'books',
